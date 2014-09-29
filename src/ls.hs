a82025042ef813e385b010ff35c39fbcb4981f0f
Volodymyr Shatsky <shockone89@gmail.com>
2014-09-29 09:50:10 +0300

    Add the pretty library.

--- src/ls.hs
+++ src/ls.hs
@@ -1,1 +1,10 @@
-main = putStrLn "Hello, World!"
+module Main where
+
+import System.Directory
+import System.Environment
+
+main :: IO ()
+main = do
+  args  <- getArgs
+  files <- getDirectoryContents (head args)
+  print files
