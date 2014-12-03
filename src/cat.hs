02f7e95e7800a32436a4876a2eeb02a8d2d3a011
Volodymyr Shatsky <shockone89@gmail.com>
2014-12-03 08:04:58 +0200

    Cat with options (prototype).

--- src/cat.hs
+++ src/cat.hs
@@ -1,7 +1,26 @@
 module Main where
 
-import System.Environment
 import Control.Monad
+import Options.Applicative
+
+type Filename = String
+data LineNumbering = WithNumbers | WithoutNumbers
+data Arguments = Arguments [Filename] LineNumbering
+
+sample :: Parser Arguments
+sample = Arguments
+  <$> some (argument str (metavar "FILES..."))
+  <*> flag WithoutNumbers WithNumbers
+      ( long "number" <> short 'n' <> help "number all output lines" )
+
+greet :: Arguments -> IO ()
+greet (Arguments filenames WithoutNumbers) = mapM_ (readFile >=> putStr) filenames
+greet (Arguments _ WithNumbers) = putStrLn "With numbers!"
 
 main :: IO ()
-main = getArgs >>= mapM_ (readFile >=> putStr)
+main = execParser opts >>= greet
+  where opts = info (helper <*> sample)
+                    ( fullDesc
+                      <> progDesc "Print a greeting for TARGET"
+                      <> header "hello - a test for optparse-applicative"
+                    )
