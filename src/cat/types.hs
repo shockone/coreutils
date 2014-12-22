b8afc76a154f5167745baf5bb18b0f462c42c66f
Volodymyr Shatsky <shockone89@gmail.com>
2014-12-22 21:20:49 +0200

    Reformat types.

--- src/cat/types.hs
+++ src/cat/types.hs
@@ -3,14 +3,15 @@ module Cat.Types where
 type Filename    = String
 type FileContent = [String]
 
-data Option = ShowAll | NumberNonBlank | ShowEnds
-            | Number | SqueezeBlank | ShowNonprintingAndTabs
-            | ShowTabs | ShowNonprinting | U
+data Option = ShowAll
+            | NumberNonBlank
+            | ShowEnds
+            | Number
+            | SqueezeBlank
+            | ShowNonprintingAndTabs
+            | ShowTabs
+            | ShowNonprinting
+            | U
             deriving (Eq)
 
 data Arguments = Arguments [Filename] [Option]
-
--- A prototype of the future refactoring.
--- class Option o where
---   parser :: Option o => Parser o
---   decorator :: FileContent -> Filecontent
