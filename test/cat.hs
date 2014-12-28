85401ab25e667ca47b87d706852caf1bbedc2bfb
Volodymyr Shatsky <shockone89@gmail.com>
2014-12-29 00:03:09 +0200

    Apply -E after squeezing empty lines.

--- test/cat.hs
+++ test/cat.hs
@@ -31,7 +31,7 @@ projectPath = "/Users/me/dev/haskell/coreutils/"
 
 params :: [[String]]
 --params = concatMap permutations $ subsequences ["-A", "-b", "-e", "-E", "-n", "-s", "-t", "-T", "-u", "-v"]
-params = concatMap permutations $ subsequences ["-n", "-s", "-T", "-b"]
+params = concatMap permutations $ subsequences ["-n", "-s", "-T", "-b", "-E"]
 
 
 providing :: [params] -> (params -> Spec) -> Spec
