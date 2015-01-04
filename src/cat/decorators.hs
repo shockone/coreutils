443524a2e6ddcb16123cca1562594c43c58c8deb
Volodymyr Shatsky <shockone89@gmail.com>
2015-01-04 23:41:49 +0200

    Unqualify BS in decorators.

--- src/cat/decorators.hs
+++ src/cat/decorators.hs
@@ -1,63 +1,74 @@
 module Cat.Decorators ( decorate ) where
 
-import Cat.Types
-import Control.Applicative ((<*>))
-import Data.Char (ord, chr)
-import qualified Data.ByteString.Char8 as BS
+import           Cat.Types
+import           Control.Applicative        ((<*>))
+import           Data.ByteString.Char8      (ByteString, append, concat,
+                                             concatMap, empty, null, pack,
+                                             singleton, split)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.ByteString.Search.DFA as S
+import           Data.Char                  (chr, ord)
+import           Data.List                  (intersperse)
+import           Prelude                    hiding (concat, concatMap, null,
+                                             unlines)
 
 
-decorate :: BS.ByteString -> Option -> BS.ByteString
+decorate :: ByteString -> Option -> ByteString
 decorate content NumberNonBlank  = splitting content (format . enumerateNonBlank)
-decorate content ShowEnds        = toStrict $ S.replace (BS.pack "\n") (BS.pack "$\n") content
+decorate content ShowEnds        = toStrict $ S.replace (pack "\n") (pack "$\n") content
 decorate content Number          = splitting content (format . enumerate)
 decorate content SqueezeBlank    = splitting content removeRepeatedBlankLines
-decorate content ShowTabs        = splitting content $ map (toStrict . S.replace (BS.pack "\t") (BS.pack "^I"))
-decorate content ShowNonprinting = splitting content $ map (BS.concatMap np)
-
-toStrict :: BL.ByteString -> BS.ByteString
-toStrict = BS.concat . BL.toChunks
-
-splitting :: BS.ByteString -> ([BS.ByteString] -> [BS.ByteString]) -> BS.ByteString
-splitting byteString f = if BS.last output == '\n' then BS.init output else output
-    where split  = BS.split '\n' byteString
-          output = BS.unlines $ f $ if BS.null (last split) then init split else split
+decorate content ShowTabs        = splitting content $ map (toStrict . S.replace (pack "\t") (pack "^I"))
+decorate content ShowNonprinting = splitting content $ map (concatMap np)
 
 
-np :: Char -> BS.ByteString
+toStrict :: BL.ByteString -> ByteString
+toStrict = concat . BL.toChunks
+
+
+splitting :: ByteString -> ([ByteString] -> [ByteString]) -> ByteString
+splitting byteString f = unlines $ f $ split '\n' byteString
+
+
+unlines :: [ByteString] -> ByteString
+unlines [] = empty
+unlines ss = concat $ intersperse nl ss
+    where nl = singleton '\n'
+
+
+np :: Char -> ByteString
 np ch
-    | ord ch >= 32 && ord ch < 127 = BS.pack [ch]
-    | ord ch == 127 = BS.pack "^?"
-    | ord ch >= 128 + 32 && ord ch < 128 + 127 = BS.pack $ "M-" ++ [chr $ ord ch - 128]
-    | ord ch >= 128 + 32 = BS.pack "M-^?"
-    | ord ch >= 32 = BS.pack $ "M-^" ++ [chr $ ord ch - 128 + 64]
-    | ch == '\t' = BS.pack [ch]
-    | ch == '\n' = BS.pack [ch] -- should break.
-    | otherwise = BS.pack $ '^':[chr $ ord ch + 64]
+    | ord ch >= 32 && ord ch < 127 = pack [ch]
+    | ord ch == 127 = pack "^?"
+    | ord ch >= 128 + 32 && ord ch < 128 + 127 = pack $ "M-" ++ [chr $ ord ch - 128]
+    | ord ch >= 128 + 32 = pack "M-^?"
+    | ord ch >= 32 = pack $ "M-^" ++ [chr $ ord ch - 128 + 64]
+    | ch == '\t' = pack [ch]
+    | ch == '\n' = pack [ch] -- should break.
+    | otherwise = pack $ '^':[chr $ ord ch + 64]
 
 
-removeRepeatedBlankLines :: [BS.ByteString] -> [BS.ByteString]
+removeRepeatedBlankLines :: [ByteString] -> [ByteString]
 removeRepeatedBlankLines [] = []
 removeRepeatedBlankLines [x] = [x]
-removeRepeatedBlankLines (x:y:rest) | BS.null x && BS.null y = removeRepeatedBlankLines (x:rest)
+removeRepeatedBlankLines (x:y:rest) | null x && null y = removeRepeatedBlankLines (x:rest)
 removeRepeatedBlankLines (x:rest) = x:removeRepeatedBlankLines rest
 
 
-enumerateNonBlank :: [BS.ByteString] -> [(Maybe Int, BS.ByteString)]
+enumerateNonBlank :: [ByteString] -> [(Maybe Int, ByteString)]
 enumerateNonBlank = numerator 1
 
 
-numerator :: Int -> [BS.ByteString] -> [(Maybe Int, BS.ByteString)]
+numerator :: Int -> [ByteString] -> [(Maybe Int, ByteString)]
 numerator _ [] = []
-numerator n (l:rest) | BS.null l = (Nothing, l) : numerator n rest
+numerator n (l:rest) | null l = (Nothing, l) : numerator n rest
                      | otherwise = (Just n, l) : numerator (n+1) rest
 
-enumerate :: [BS.ByteString] -> [(Maybe Int, BS.ByteString)]
+enumerate :: [ByteString] -> [(Maybe Int, ByteString)]
 enumerate = zip ([Just] <*> [1..])
 
 
-format :: [(Maybe Int, BS.ByteString)] -> [BS.ByteString]
+format :: [(Maybe Int, ByteString)] -> [ByteString]
 format numbersWithRows = map (formatLine padding) numbersWithRows
   where padding = calculatePadding $ fromIntegral $ length numbersWithRows
 
@@ -66,14 +77,14 @@ calculatePadding :: Double -> Int
 calculatePadding = ceiling . log
 
 
-formatLine :: Int -> (Maybe Int, BS.ByteString) -> BS.ByteString
-formatLine paddingWidth (lineNumber, line) = BS.append (pad lineNumber paddingWidth) line
+formatLine :: Int -> (Maybe Int, ByteString) -> ByteString
+formatLine paddingWidth (lineNumber, line) = append (pad lineNumber paddingWidth) line
 
 
-pad :: Maybe Int -> Int -> BS.ByteString
-pad Nothing _ = BS.pack ""
-pad (Just number) maxPaddingWidth = foldl1 BS.append [padding, BS.pack shownNumber, BS.pack "\t"]
+pad :: Maybe Int -> Int -> ByteString
+pad Nothing _ = pack ""
+pad (Just number) maxPaddingWidth = foldl1 append [padding, pack shownNumber, pack "\t"]
   where
-    padding = BS.pack $ replicate paddingWidth ' '
+    padding = pack $ replicate paddingWidth ' '
     paddingWidth = (maxPaddingWidth - length shownNumber)
     shownNumber = show number
